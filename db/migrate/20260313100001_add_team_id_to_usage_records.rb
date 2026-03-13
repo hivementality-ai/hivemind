750a0142546f2b0d98632265261f22862c000dc6
matthewsuttles <matthew.suttles@noldor.com>
2026-03-13 16:50:41 -0500

    lint

--- db/migrate/20260313100001_add_team_id_to_usage_records.rb
+++ db/migrate/20260313100001_add_team_id_to_usage_records.rb
@@ -3,6 +3,6 @@
 class AddTeamIdToUsageRecords < ActiveRecord::Migration[8.1]
   def change
     add_reference :usage_records, :team, foreign_key: true, null: true
-    add_index :usage_records, [:team_id, :created_at]
+    add_index :usage_records, [ :team_id, :created_at ]
   end
 end
