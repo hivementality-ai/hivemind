5508eff0e0eece95f5e2169d2770889455978b8b
matthewsuttles <matthewsuttles13@yahoo.com>
2026-06-30 10:56:40 -0500

    feat(channels): add Mattermost channel adapter

--- app/services/channels/registry.rb
+++ app/services/channels/registry.rb
@@ -9,7 +9,8 @@ module Channels
       "whatsapp" => "Channels::WhatsappAdapter",
       "signal" => "Channels::SignalAdapter",
       "matrix" => "Channels::MatrixAdapter",
-      "email" => "Channels::EmailAdapter"
+      "email" => "Channels::EmailAdapter",
+      "mattermost" => "Channels::MattermostAdapter"
     }.freeze
 
     # Backwards compatibility alias
